fb2eb3b2b2f41bf5860cb1897e1c8c75f7f85a37
Mostafa Tawfiq <mostafa.mohammad.twfiq@gmail.com>
2020-09-16 05:03:27 +0200

    Seperated the database info into final strings in DataBaseConnection class.

--- src/DataBase/DataBaseConnection.java
+++ src/DataBase/DataBaseConnection.java
@@ -8,14 +8,18 @@ public abstract class DataBaseConnection {
 
     private static Connection connection = null;
 
+    private static final String dataBaseName = "todolistdb";
+    private static final String dataBaseUserName = "todoListApp";
+    private static final String dataBaseUserPass = "pass";
+
     public static Connection createDataBaseConnection () throws SQLException {
 
         if (connection == null) {
 
             connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/todolistdb",
-                    "todoListApp",
-                    "pass"
+                    "jdbc:mysql://localhost:3306/" + dataBaseName,
+                    dataBaseUserName,
+                    dataBaseUserPass
             );
 
         }
